64f578c1530525fc6f3c1039b61d4cc6146fd84a
Marco-Venerando <marco.venera@gmail.com>
2026-01-19 16:39:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+const teamContainer = document.getElementById("team-container");
+
+teamMembers.forEach((member) => {
+  teamContainer.innerHTML += `
+    <div class="card">
+      <img src="${member.img}" alt="${member.name}">
+      <h3>${member.name}</h3>
+      <p><strong>${member.role}</strong></p>
+      <p>${member.email}</p>
+    </div>
+  `;
+});
